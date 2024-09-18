7ff23b06d89e22cb513df49135c6dd39c03eb565
Esteban Sahid <esteban_ct28@hotmail.com>
2024-09-18 22:25:56 +0200

    Funcional hasta listado de productos login/logout y buscador

--- js/sheets.js
+++ js/sheets.js
@@ -1,5 +1,8 @@
-// Array vacío para almacenar productos
+/* Listar y Buscador */
+// Variables
 let productsData = [];
+
+document.getElementById("warning_lbl").style.visibility = "Hidden";
 
 async function getProducts() {
     let response;
@@ -17,11 +20,13 @@ async function getProducts() {
     if (!range || !range.values || range.values.length == 0) {
         console.warn("No se encontraron valores");
         alert("No se encontraron productos");
-        return;
-    }
+        document.getElementById("warning_lbl").innerText = "No se encontraron productos";
+    } 
 
     // Almacenamos los productos y los mostramos inicialmente
     productsData = range.values;
+    console.log("Products Data");
+    console.log(productsData);
     displayProducts(productsData);
 }
 
@@ -29,26 +34,40 @@ function displayProducts(data) {
     const tbody = document.getElementById("content");
     tbody.innerHTML = '';
 
-    data.forEach((row) => {
-        const tr = document.createElement("tr");
-        row.forEach((cell) => {
-            const td = document.createElement("td");
-            td.textContent = cell;
-            tr.appendChild(td);
+    console.log("Datos N");
+    console.log(data.length);
+
+    if (data.length > 15) {
+        document.getElementById("warning_lbl").innerText = "Utiliza el buscador para filtrar los productos";
+        document.getElementById("warning_lbl").style.visibility = "visible";
+    } else if (data.length == 0) {
+        document.getElementById("warning_lbl").innerText = "No se encontraron productos";
+    } else {
+        document.getElementById("warning_lbl").style.visibility = "Hidden";
+        data.forEach((row) => {
+            const tr = document.createElement("tr");
+            row.forEach((cell) => {
+                const td = document.createElement("td");
+                td.textContent = cell;
+                tr.appendChild(td);
+            });
+            tbody.appendChild(tr);
         });
-        tbody.appendChild(tr);
-    });
+    }
+
 }
 
 function searchProducts() {
     const input = document.getElementById("searchInput").value.toLowerCase();
-    
+    console.log("input ", input)
     // Filtrar productos por PLUS o nombre de producto
     const filteredData = productsData.filter((row) => {
-        const plus = row[1].toLowerCase();
-        const producto = row[2].toLowerCase();
+        console.log(row);
+        const plus = row[0].toLowerCase();
+        const producto = row[1].toLowerCase();
+        console.log(plus, " ", producto)
         return plus.includes(input) || producto.includes(input);
     });
 
-    displayProducts(filteredData); // Actualizar la tabla con los resultados filtrados
+    displayProducts(filteredData);
 }
